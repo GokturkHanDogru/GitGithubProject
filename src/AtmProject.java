a18ca5cb06bd561aa4e06d610f1a8b414db93859
Göktürk Han Dogru <gokturkhandogru@gmail.com>
2023-04-26 21:00:13 +0200

    v2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -2,5 +2,7 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("This is an ATM Project");
         //Version1
+        System.out.println("Java is a good coding Language");
+        //Version2
     }
 }
